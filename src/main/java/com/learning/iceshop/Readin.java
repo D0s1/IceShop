ff16d9e4576b354a8966c3650cfe7fbd565853ba
d0s1 <dominic1020@web.de>
2025-11-20 12:52:29 +0100

    big cleanup

--- src/main/java/com/learning/iceshop/Readin.java
+++ src/main/java/com/learning/iceshop/Readin.java
@@ -14,48 +14,14 @@ import java.util.function.Predicate;
 public class Readin {
 
     private final IceSortRepository iceSortRepository;
-    private final CsvData csvData;
 
-    public Readin(IceSortRepository iceSortRepository, CsvData csvData) {
+    public Readin(IceSortRepository iceSortRepository) {
         this.iceSortRepository = iceSortRepository;
-        this.csvData = csvData;
     }
 
-
-
-    @GetMapping
-    public List<String[]> getAllRecords(@ModelAttribute CsvFilter filter) {
-        return csvData.getRecordsAsString(filter);
-    }
-
-    @GetMapping("/sorte")
-    public List<String> getSorte(@ModelAttribute CsvFilter filter) {
-        return csvData.getColumnA(filter);
-    }
-
-    @GetMapping("/score")
-    public List<Integer> getScore(@ModelAttribute CsvFilter filter) {
-        return csvData.getColumnB(filter);
-    }
-
-    @PatchMapping("/records/{id}/zutaten")
-    public IceDataset addZutat(
-            @PathVariable String id,
-            @RequestParam String zutat
-    ) {
-        IceDataset record = csvData.getRecords().stream().filter(elem -> elem.getSorte().equals(id)).findFirst().orElse(null);
-        assert record != null;
-        record.getZutaten().add(zutat);
-        return record;
-    }
     @GetMapping("/db")
-    public List<IceSort> getAllIceSOrt(@ModelAttribute IceSortFilter filter){
+    public List<IceSort> getAllIceSort(@ModelAttribute IceSortFilter filter){
         return iceSortRepository.findAll(IceSortSpecs.buildSpec(filter));
-    }
-
-    @GetMapping("/scoreabove{id}")
-    public List<IceSort> getScoreAbove(@PathVariable int id){
-        return iceSortRepository.findByScoreGreaterThan(id);
     }
 
     @GetMapping("/filter/names")
